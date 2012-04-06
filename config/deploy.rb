574dba57bf2b534e5692421aa53d48461e0c2a72
Dmitry Trager <dmitry.trager@flatstack.com>
2012-04-06 23:56:03 +0400

    Deployment configuration files added, some tune up

---
+++ config/deploy.rb
@@ -0,0 +1,42 @@
+require 'bundler/capistrano'
+
+server "78.47.150.54", :web, :app, :db, primary: true
+
+set :application, "copycopter-server"
+set :user, "deployer"
+set :deploy_to, "/home/#{user}/code/#{application}"
+set :deploy_via, :remote_cache
+set :use_sudo,  false
+
+set :scm, :git
+set :repository, "git@github.com:dmitrytrager/#{application}.git"
+set :branch, :master
+
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+
+after "deploy", "deploy:cleanup"
+
+namespace :deploy do
+  task :setup_config, roles: :app do
+    run "mkdir -p #{shared_path}/config"
+    put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
+    puts "Now edit the config files in #{shared_path}."
+  end
+  after "deploy:setup", "deploy:setup_config"
+
+  task :symlink_config, roles: :app do
+    sudo "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
+  after "deploy:finalize_update", "deploy:symlink_config"
+
+  desc "Make sure local git is in sync with remote"
+  task "check_revision", roles: :web do
+    unless `git rev-parse HEAD` == `git rev-parse origin/master`
+      puts "WARNING: HEAD is not the same as origin/master"
+      puts "Run `git push` to sync changes."
+      exit
+    end
+  end
+  before "deploy", "deploy:check_revision"
+end
