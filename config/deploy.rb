b80d7a240cb81d8c110265c5319ee33fddc3c7d7
Dmitry Trager <dmitry.trager@flatstack.com>
2016-09-12 17:50:41 +0300

    Replace redundant tasks with linked options

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,14 +14,14 @@ server "78.47.50.178",
   primary: true
 
 set :scm, :git
-# set :repository, "git@github.com:dmitrytrager/#{application}.git"
+# set :repository, "git@github.com:dmitrytrager/#{fetch(:application)}.git"
 set :repo_url, "git@github.com:dmitrytrager/#{fetch(:application)}.git"
 # set :branch, :master
 ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
 set :conditionally_migrate, true
+set :pry, true
 
-# default_run_options[:pty] = true
 # ssh_options[:forward_agent] = true
 set :ssh_options, {
   forward_agent: true,
@@ -29,7 +29,7 @@ set :ssh_options, {
   user: "deploy",
 }
 
-# set :linked_files, fetch(:linked_files, []).push("config/database.yml")
+set :linked_files, fetch(:linked_files, []).push("config/database.yml")
 set :linked_dirs,
   fetch(:linked_dirs, []).push(
     "log",
@@ -46,32 +46,19 @@ after "deploy:publishing", "deploy:restart"
 namespace :deploy do
   task :start do
   end
+
   task :stop do
   end
+
   task :restart do
     on roles(:app), except: { no_release: true } do
       invoke "unicorn:reload"
     end
-
-  task :setup_config do
-    on roles(:app) do
-      run "mkdir -p #{shared_path}/config"
-      put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
-      puts "Now edit the config files in #{shared_path}."
-    end
   end
-  after "deploy:setup", "deploy:setup_config"
-
-  task :symlink_config do
-    on roles(:app) do
-      run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-    end
-  end
-  after "deploy:finalize_update", "deploy:symlink_config"
 
   desc "Make sure local git is in sync with remote"
   task "check_revision" do
-    on roles(:web)
+    on roles(:web) do
       unless `git rev-parse HEAD` == `git rev-parse origin/master`
         puts "WARNING: HEAD is not the same as origin/master"
         puts "Run `git push` to sync changes."
